481ad179fbf420116e0f5e779bb3e33a99134095
Mordred Admin <gogs@fake.local>
2018-08-21 14:11:46 +0200

    This should stay 32...

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -4,7 +4,7 @@ if(UNIX)
 #  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
   # here is the target environment located
 else()
-  add_definitions(-D_WIN64_WINNT=0x601)
+  add_definitions(-D_WIN32_WINNT=0x601)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHc")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
