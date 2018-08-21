8b2d44cff7ceeb73849401bfe44464f864a08d1d
Mordred Admin <gogs@fake.local>
2018-08-21 13:58:37 +0200

    Test with different arch

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -4,14 +4,20 @@ if(UNIX)
 #  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
   # here is the target environment located
 else()
-  add_definitions(-D_WIN32_WINNT=0x601)
+  #add_definitions(-D_WIN32_WINNT=0x601)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHc")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
+  if(${CMAKE_SIZEOF_VOID_P} EQUAL "8")
+    set (PROJECTNAME_ARCHITECTURE "x64")
+  else ()
+    set (PROJECTNAME_ARCHITECTURE "x86")
+  endif ()    # disabling SAFESEH
 
   if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    # disabling SAFESEH
+
+
     message(STATUS "Disabling Safe Exception Handlers..")
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /SAFESEH:NO")
 
